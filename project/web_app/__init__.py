779ff6754cff944ca915b325eebbe7d1faeffec1
britneh <brhacket@gmail.com>
2020-06-17 14:16:44 -0700

    added SQL functionality

--- project/web_app/__init__.py
+++ project/web_app/__init__.py
@@ -1,7 +1,8 @@
+from web_app.models import db, migrate
 from flask import Flask
 from web_app.routes.home_routes import home_routes
 
-from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 DATABASE_URI = "sqlite:///twitbrit.db" #creating database 
@@ -11,6 +12,9 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATION"] =False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
 
